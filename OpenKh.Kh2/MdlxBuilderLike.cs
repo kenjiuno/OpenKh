54dcadbf864f562880308d64379475008b79b114
kenjiuno <h8mastre@gmail.com>
2022-08-22 14:11:13 +0900

    Adding MdlxBuilderLike inside OpenKh.Kh2

---
+++ OpenKh.Kh2/MdlxBuilderLike.cs
@@ -0,0 +1,17 @@
+using OpenKh.Ps2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenKh.Kh2
+{
+    public class MdlxBuilderLike
+    {
+        public MdlxBuilderLike()
+        {
+            var dummy = new VifUnpacker(new byte[0]);
+
+            throw new NotImplementedException();
+        }
+    }
+}
